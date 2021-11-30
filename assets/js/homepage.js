7b07cb478dd722a48413a6b134b37c7d73a505d9
IanPrice <ianadamprice@gmail.com>
2021-11-30 13:50:12 -0500

    finish get user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,11 +3,49 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
+    fetch(apiUrl)
+      .then(function(response) {
+          // request was successful
+        if (response.ok) {
+          response.json().then(function(data) {
+            displayRepos(data, user);
+          });
+        } else {
+          alert("Error: GitHub User Not Found");
+        }
+    })
+    .catch(function(error) {
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to GitHub");
     });
+};
+
+  var userFormEl = document.querySelector("#user-form");
+  var nameInputEl = document.querySelector("#username");
+
+  var formSubmitHandler = function(event) {
+      event.preventDefault();
+      
+      // get value from input element 
+      var username = nameInputEl.value.trim();
+
+      if (username) {
+          getUserRepos(username);
+          nameInputEl.value = "";
+      } else {
+          alert("Please enter a GitHub username");
+      }
+
+      console.log(event);
+  };
+
+  var displayRepos = function(repos,searchTerm) {
+      if (repos.length === 0) {
+          repoContainerEl.textContent = "No repositories found.";
+          return;
+      }
+      console.log(repos);
+      console.log(searchTerm);
   };
   
-  getUserRepos();
+  userFormEl.addEventListener("submit", formSubmitHandler);
